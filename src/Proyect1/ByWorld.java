81a54117b40d1801ca7d8f3713cf0c323e961241
Ameyali Miranda <ame.gm1806@gmail.com>
2023-01-24 10:09:41 -0600

    Comienzo del proyecto

---
+++ src/Proyect1/ByWorld.java
@@ -0,0 +1,7 @@
+package Proyect1;
+
+public class ByWorld {
+    public static void main (String [] args){
+        System.out.println("ADIOS MUNDO");
+    }
+}
---
+++ src/Proyect1/proyect1.java
@@ -0,0 +1,34 @@
+package Proyect1; // nombre de la empresa en la que trabajemos
+
+/*
+class indica que es una clase
+public indica visibilidad
+ */
+public class proyect1 {
+
+    /*
+    Crear un método main que sirve de punto de entrada principal
+    a lo que seria una aplicacion de java, el metodo contiene una
+    serie de modificadores:
+    *public -> publico, puede sergit init ejecutado desde fuera de la clase
+    *static -> pertenece a la clase proyect1
+    *void -> el tipo de retorno (no devuelve nada, solamente ejecutara un código)
+    *main -> nombre que indicara que este es el metodo principal
+    *String [] args -> array de argumentos podria recibir datos por la entrada
+    estandar.
+    */
+
+    /**
+     * Esto es una documentación del código
+     * @param args
+     */
+
+    /*
+    Imprime a través de terminal
+     */
+    public static void main(String [] args){
+
+        System.out.println("HOLA MUNDO");
+    }
+
+}
